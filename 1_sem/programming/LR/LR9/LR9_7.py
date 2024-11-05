645d4f15d9860c1459801395139086e8f8f92f76
unre <unreelcy@gmail.com>
2024-11-05 21:49:25 +0300

    forgot about lr9_7

--- 1_sem/programming/LR/LR9/LR9_7.py
+++ 1_sem/programming/LR/LR9/LR9_7.py
@@ -10,8 +10,52 @@
 """
 
 def main():
-    pass
+    # 0:
+    x, y, z = (int(i) for i in input('Введите 3 размера трехмерного массива через пробел\n> ').split())
+
+    mega_matrix = []
+
+    for x_ind in range(x):
+        matrix = []
+        for y_ind in range(y):
+            array = [int(i) for i in input(f'Введите {y_ind} строку {x_ind} матрицы\n> ').strip().split()]
+            matrix.append(array)
+        mega_matrix.append(matrix)
+
+    # for matrix in mega_matrix:
+    #     print('\n')
+    #     print(*matrix, sep='\n')
+
+    # 1:
+    max_d = 0
+
+    if x > y:
+        if x > z:
+            max_d = x
+        else:
+            max_d = z
+    elif y > z:
+        max_d = y
+    else:
+        max_d = z
+
+    max_d = (max_d - 1) // 2
+
+    for matrix in mega_matrix:
+        print(matrix[max_d])
 
 
 if __name__ == '__main__':
     main()
+'''
+1 1 1
+2 2 2
+3 3 3
+4 4 4
+5 5 5
+6 6 6
+7 7 7
+8 8 8
+9 9 9
+
+'''
