1e127f0610fa1e3369e28e419c390320bca917cb
unre <unreelcy@gmail.com>
2025-01-10 08:49:09 +0300

    little tricking with files

--- 1_sem/programming/seminars/test.py
+++ 1_sem/programming/seminars/test.py
@@ -1,8 +1,13 @@
-f1 = lambda a, b, c: (not ((a and (not b)) or (b and (not c))) and (((a and b) or (b and c)) <= ((not a) or (b and c))))
-f2 = lambda a, b, c: (((not a) and (not b)) or ((not a) and c) or (b and c))
-print('a b c | f1 f2')
-for a in range(2):
-    for b in range(2):
-        for c in range(2):
-            print(a, b, c, "|", int(f1(a, b, c)), int(f2(a, b, c)))
+# f1 = lambda a, b, c: (not ((a and (not b)) or (b and (not c))) and (((a and b) or (b and c)) <= ((not a) or (b and c))))
+# f2 = lambda a, b, c: (((not a) and (not b)) or ((not a) and c) or (b and c))
+# print('a b c | f1 f2')
+# for a in range(2):
+#     for b in range(2):
+#         for c in range(2):
+#             print(a, b, c, "|", int(f1(a, b, c)), int(f2(a, b, c)))
                                              
+
+with open("/home/unre/PycharmProjects/bmstu_labs/1_sem/programming/seminars/in.txt", 'r') as f:
+    for _ in range(7):
+        print(f.read(1))
+    print(f.readline())
